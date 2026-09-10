919f68970860994e14893cd61f421b48f28feffc
Ö. Zeynep Güner Yılmaz <ozdezeynepg@gmail.com>
2026-09-10 13:34:22 +0300

    Fix package docstring syntax

--- backend/v2/__init__.py
+++ backend/v2/__init__.py
@@ -2,7 +2,7 @@
 
 The ``v2`` directory name is retained only for provenance of the publication
 implementation; users interact with one application, InterfaceScout.
-""
+"""
 
 from .model_settings import MODEL_VERSION
 from .interface_engine import analyze_all_maps
